d7454450db425cc1ac6cb8b28b261133b1dd3b94
chagas-junior <chagas.junior@alu.ufc.br>
2024-04-24 20:51:24 -0300

    Criando a requirements

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ st.write()
 df = pd.read_csv("https://raw.githubusercontent.com/hermeson883/data_science_workshop/main/linguagens.csv")
 
 st.dataframe(df)
+
+lista_escolhas = df["Linguagem"].unique()
+
+escolha = st.selectbox('Escolha uma alternativa',lista_escolhas)
+
+botao = st.button("Clique para enviar")
+
+if(botao):
+    st.dataframe(df.loc[df["Linguagem"] == escolha])
+
